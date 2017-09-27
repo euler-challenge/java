4c2b7693f972a9650038b34df5842131c4c1e21a
Sparkling Comet <apps@shanerx.org>
2017-09-27 16:46:12 +0200

    Solve #1

---
+++ solutions/src/main/java/org/shanerx/euler/prob1/Main.java
@@ -0,0 +1,13 @@
+package org.shanerx.euler.prob1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 1;
+        while (n <= 1000) {
+            if (n % 3 == 0 || n % 5 == 0)
+                System.out.printf("%d" + (n == 1000 ? "" : " , "), n);
+            n++;
+        }
+    }
+}
